2f13d9e18916219cc843d9050d9d755011ce9271
Michał Dacko <michaldacko4@wp.pl>
2024-11-07 19:19:32 +0100

    fix supabase migrations and seed.sql

--- supabase/migrations/20241107170929_add_default_avatar_column.sql
+++ supabase/migrations/20241107170929_add_default_avatar_column.sql
@@ -1,7 +1,8 @@
+alter table "public"."Country" enable row level security;
+
 create type "public"."DefaultAvatarVariant" as enum ('beam', 'marble', 'pixel', 'sunset', 'ring', 'bauhaus');
 
 alter table "public"."UserProfile" add column "avatar_url" text;
 
 alter table "public"."UserProfile" add column "default_avatar_type" "DefaultAvatarVariant" default 'beam'::"DefaultAvatarVariant";
-
 
